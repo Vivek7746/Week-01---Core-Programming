1ce76919c4d840a0a75cc1cd2fb8cc3566009717
Vivek <vivekthegamer999@gmail.com>
2025-01-12 20:14:37 +0530

    Sum using For loop Program

---
+++ level1/ForNaturalSum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class ForNaturalSum{
+	public static void main(String[] args){
+		
+		Scanner input = new Scanner(System.in);
+			
+			// Creating a variable Natural number
+			int natural = 0;
+			
+			// Taking input from the user
+			natural = input.nextInt();
+			
+			// Finding sum using For loop
+			int LoopSum = 0;
+			for(int tempNatural = natural; tempNatural >= 1; tempNatural--){
+				LoopSum += tempNatural;
+			}
+			
+			// Finding sum using Maths formula
+			int FormulaSum = (natural * (natural+1))/2;
+			
+			System.out.println("Sum of Natural number using while loop is " + LoopSum);
+			System.out.println("Sum using Formula is " + FormulaSum);
+	}
+}
