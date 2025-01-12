90ed4b8bb3ec72aa5b63e25b744d674f8cff5769
Vivek <vivekthegamer999@gmail.com>
2025-01-12 18:59:14 +0530

    Find Age Program

---
+++ level1/findAge.java
@@ -0,0 +1,15 @@
+class findAge{
+	public static void main(String[] args){
+		
+		// Birth year of Harry
+		int birthAge = 2000; 
+		
+		// Current year 
+		int currentYear = 2024;
+		
+		// Harry's age will be the difference of his birth year
+		// and current year 
+		
+		System.out.print("Harry's age in 2024 is " + (currentYear - birthAge));
+	}
+}
